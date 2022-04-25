c95534075bc4de240090825b7f3d9b88bdb3f778
vanta blanta <vantablanta@gmail.com>
2022-04-25 12:25:24 +0300

    api link added

--- app/views.py
+++ app/views.py
@@ -1,12 +1,23 @@
-from email import message
 from flask import render_template
 
 from app import app
 
 @app.route('/')
 def index():
-    message = 'Hello World'
-    
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Home - Welcome to The best Movie Review Website Online'
+
     return render_template('home.html', 
-        message = message 
+       title = title
     )
+
+@app.route('/movie/<movie_id>')
+def movie(movie_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    title = f'You are viewing {movie_id}'
+    return render_template('movie.html',title = title)
