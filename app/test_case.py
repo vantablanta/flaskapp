d1566ccb6eff3d61af035dce4fefe9c873206c7d
vanta blanta <vantablanta@gmail.com>
2022-04-26 07:13:15 +0300

    setup testcase

--- app/test_case.py
+++ app/test_case.py
@@ -6,15 +6,17 @@ class MovieTest(unittest.TestCase):
     '''
     Test Class to test the behaviour of the Movie class
     '''
-
+    new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+    
     def setUp(self):
         '''
         Set up method that will run before every Test
         '''
         self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+    
     def setUp(self):
         """"""
-        # self.assertEqual(Movie.id, 1234)
+        self.assertEqual(self.new_movie.id, 1234)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_movie,Movie))
